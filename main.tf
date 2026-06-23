240321c70304dcb16fbeed802e7fd25e20d4c1b7
dkchcl <dkc.hcl@gmail.com>
2026-06-23 23:01:36 +0530

    update modules

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "public_ip" {
 module "avm-res-compute-virtualmachine" {
   source  = "Azure/avm-res-compute-virtualmachine/azurerm"
   version = "0.21.0"
-  name = "avm-res-compute-virtualmachine"
+  name = "avm-res-vm1"
   location = "East US"
   resource_group_name = "dev-rg-01"
   zone = "1"
